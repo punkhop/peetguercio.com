9f76cae0ff065021a2fb9e549eaeaa4c84ea0410
Geoffrey Plitt <geoff@gweb.org>
2014-10-26 16:39:27 -0700

    calendar stuff in progress

---
+++ public/js/calendar.js
@@ -0,0 +1,25 @@
+google.setOnLoadCallback(getMyFeed);
+var myService;
+var feedUrl = "https://www.google.com/calendar/feeds/g27.net_9ht2e4uhfh602mnen6psa6mun0%40group.calendar.google.com/public/basic";
+
+function setupMyService() {
+  myService = new google.gdata.calendar.CalendarService('exampleCo-exampleApp-1');
+}
+
+function getMyFeed() {
+  setupMyService();
+  myService.getEventsFeed(feedUrl, handleMyFeed, handleError);
+}
+
+function handleMyFeed(result) {
+  var entries = result.feed.entry.slice(0,10);
+  for (var i = 0; i < entries.length; i++) {
+      var calendarEntry = entries[i];
+      var calendarTitle = calendarEntry.getTitle().getText();
+      console.log(calendarTitle);
+    }
+  }
+
+function handleError(err) {
+  console.log('calendar error: ', err);
+}
