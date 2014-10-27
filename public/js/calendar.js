ea0b05ad4a48990ed246c19f05ecb4bf39496d6c
Geoffrey Plitt <geoff@gweb.org>
2014-10-26 17:47:43 -0700

    got start times

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,22 +1,24 @@
 google.setOnLoadCallback(getMyFeed);
 var myService;
-var feedUrl = "https://www.google.com/calendar/feeds/g27.net_9ht2e4uhfh602mnen6psa6mun0%40group.calendar.google.com/public/basic";
-
+var feedUrl = "https://www.google.com/calendar/feeds/g27.net_9ht2e4uhfh602mnen6psa6mun0%40group.calendar.google.com/public/full?alt=json-in-script&singleevents=true&futureevents=true";
 function setupMyService() {
-  myService = new google.gdata.calendar.CalendarService('exampleCo-exampleApp-1');
+  myService = new google.gdata.calendar.CalendarService('gdata-js-client-samples-simple');
 }
 
 function getMyFeed() {
   setupMyService();
-  myService.getEventsFeed(feedUrl, handleMyFeed, handleError);
+  var query = new google.gdata.calendar.CalendarEventQuery(feedUrl);
+  //query.setSingleEvents(true);
+  myService.getEventsFeed(query, handleMyFeed, handleError);
 }
 
 function handleMyFeed(result) {
   var entries = result.feed.entry.slice(0,10);
   for (var i = 0; i < entries.length; i++) {
       var entry = entries[i];
-      var title = entry.getTitle().getText();
-      console.log(title);
+      var date = entry.getTimes()[0].startTime;
+      //var title = entry.getTitle().getText();
+      console.log(date);
     }
   }
 
