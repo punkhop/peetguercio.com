4bf910d9c4fc7d28a2b6823045ded0cedd4064a3
Geoffrey Plitt <geoff@gweb.org>
2014-10-26 17:58:29 -0700

    finished calendar integration

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,6 +1,6 @@
 google.setOnLoadCallback(getMyFeed);
 var myService;
-var feedUrl = "https://www.google.com/calendar/feeds/g27.net_9ht2e4uhfh602mnen6psa6mun0%40group.calendar.google.com/public/full?alt=json-in-script&singleevents=true&futureevents=true";
+var feedUrl = "https://www.google.com/calendar/feeds/g27.net_9ht2e4uhfh602mnen6psa6mun0%40group.calendar.google.com/public/full?futureevents=true"; // singleevents=true
 function setupMyService() {
   myService = new google.gdata.calendar.CalendarService('gdata-js-client-samples-simple');
 }
@@ -13,12 +13,20 @@ function getMyFeed() {
 }
 
 function handleMyFeed(result) {
+  // get all calendar entries
   var entries = result.feed.entry.slice(0,10);
   for (var i = 0; i < entries.length; i++) {
+      // format with date and title
       var entry = entries[i];
-      var date = entry.getTimes()[0].startTime;
-      //var title = entry.getTitle().getText();
-      console.log(date);
+      var date = new Date(entry.getTimes()[0].startTime);
+      var title = entry.getTitle().getText();
+      var formatted = (date.getMonth()+1) + '/' + date.getDate() + ' - ' + title;
+      //console.log(date);
+      //console.log(formatted);
+      //console.log('---');
+
+      // add to UI.
+      $('#shows').append('<h4>' + formatted + '</h4>')
     }
   }
 
